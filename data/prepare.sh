76ddf25f0f7d9b80962127165cb16fae8be436d1
Rik <rbose@cs.rochester.edu>
2020-02-24 23:17:30 -0500

    preparing to display images

---
+++ data/prepare.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Create dir $2
+# for each sentence, add a file $2/i with parse, alt1, alt2
+mkdir $2
+index=1
+base=http://raw.githubusercontent.com/mrmechko/trips-annotate/master/$2/
+echo id,reference,img_1,img_2 > ${2}/data.csv
+while read sentence; do
+    mkdir $2/$index
+    trips-cli query -n 3 --text "'$sentence'" > $2/$index/parse.json
+    trips-cli render -p $2/$index/parse.json -f svg -o $2/$index/best
+    trips-cli render -p $2/$index/parse.json -f svg -o $2/$index/first -a 0
+    trips-cli render -p $2/$index/parse.json -f svg -o $2/$index/second -a 1
+    echo $index,${base}${index}/best.svg\?sanitize=true,${base}${index}/first.svg\?sanitize=true,${base}${index}/second.svg\?sanitize=true >> ${2}/data.csv
+    ((index+=1))
+done < $1
+
+mlr --c2j --jlistwrap cat $2/data.csv | jq > $2/data.json
