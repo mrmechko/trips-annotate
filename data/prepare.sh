f4c150ea600178bf2c3bb79ea12191f3978ca0c0
Rik <rbose@cs.rochester.edu>
2020-02-25 22:18:42 -0500

    changed style

--- data/prepare.sh
+++ data/prepare.sh
@@ -9,9 +9,9 @@ echo id,reference,img_1,img_2 > ${2}/data.csv
 while read sentence; do
     mkdir $2/$index
     trips-cli query -n 3 --text "'$sentence'" > $2/$index/parse.json
-    trips-cli render -p $2/$index/parse.json -f svg -o $2/$index/best
-    trips-cli render -p $2/$index/parse.json -f svg -o $2/$index/first -a 0
-    trips-cli render -p $2/$index/parse.json -f svg -o $2/$index/second -a 1
+    trips-cli render -p $2/$index/parse.json -f svg -o $2/$index/best -s style.json
+    trips-cli render -p $2/$index/parse.json -f svg -o $2/$index/first -a 0 -s style.json
+    trips-cli render -p $2/$index/parse.json -f svg -o $2/$index/second -a 1 -s style.json
     echo $index,${base}${index}/best.svg\?sanitize=true,${base}${index}/first.svg\?sanitize=true,${base}${index}/second.svg\?sanitize=true >> ${2}/data.csv
     ((index+=1))
 done < $1
